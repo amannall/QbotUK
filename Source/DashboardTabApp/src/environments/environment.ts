84c994f56b7166bc65b9e0ba4982106147ad5a83
amannall <51120998+amannall@users.noreply.github.com>
2020-09-09 17:48:36 +0100

    Update environment.ts

--- Source/DashboardTabApp/src/environments/environment.ts
+++ Source/DashboardTabApp/src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotimperial.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://icqbot.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
         tenantId: "2b897507-ee8c-4575-830b-4f8267c3d307",
-        clientId: "0f27d84a-58a7-4da4-9c79-227ceaffc62a",
+        clientId: "cad086b8-a92d-4373-bdd2-88bf30b4db13",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
