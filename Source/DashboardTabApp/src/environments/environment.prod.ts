258276942f8042b48489e2eabaa274cad56696ee
amannall <51120998+amannall@users.noreply.github.com>
2020-09-09 13:36:32 +0100

    Update environment.prod.ts
    Imperial tenent change

--- Source/DashboardTabApp/src/environments/environment.prod.ts
+++ Source/DashboardTabApp/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qboteduukunicontoso.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://qbotimperial.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "9e4d7ef0-9135-47fc-b4cd-a6cbc485e0bd",
-        clientId: "bdd735c4-91e4-400c-a5ef-651ee712bdea",
+        tenantId: "2b897507-ee8c-4575-830b-4f8267c3d307",
+        clientId: "0f27d84a-58a7-4da4-9c79-227ceaffc62a",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
