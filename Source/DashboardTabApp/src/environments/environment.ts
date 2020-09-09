37ee4b97f5adcf64d1540966230a6434264d42c1
amannall <51120998+amannall@users.noreply.github.com>
2020-09-09 13:36:25 +0100

    Update environment.ts
    Imperial tenent change

--- Source/DashboardTabApp/src/environments/environment.ts
+++ Source/DashboardTabApp/src/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "9e4d7ef0-9135-47fc-b4cd-a6cbc485e0bd",
-        clientId: "bdd735c4-91e4-400c-a5ef-651ee712bdea",
+        tenantId: "2b897507-ee8c-4575-830b-4f8267c3d307",
+        clientId: "0f27d84a-58a7-4da4-9c79-227ceaffc62a",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
